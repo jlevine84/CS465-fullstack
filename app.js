1cb95b1684e0e7b212315b749b6ce6a80f5b2381
Jeff Levine <jlevine84@yahoo.com>
2026-05-13 09:24:25 -0400

    Module 2 completed baseline

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
+// Reqs
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var handlebars = require('hbs')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// App routes
+var indexRouter = require('./app_server/routes/index');
+var usersRouter = require('./app_server/routes/users');
+var travelRouter = require('./app_server/routes/travel')
 
+// Init express
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+
+// Register handlebar partials
+handlebars.registerPartials(__dirname + '/app_server/views/partials')
+
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
@@ -21,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/travel', travelRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
