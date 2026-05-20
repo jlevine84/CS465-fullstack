76e4d882bf58160933319ba56cbbb7e45e9fe2cd
Jeff Levine <jlevine84@yahoo.com>
2026-05-20 11:53:09 -0400

    Updated index hb and added routes to express

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ var handlebars = require('hbs')
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var travelRouter = require('./app_server/routes/travel')
+var roomsRouter = require('./app_server/routes/rooms')
+var newsRouter = require('./app_server/routes/news')
+var mealsRouter = require('./app_server/routess/meals')
+var aboutRouter = require('./app_server/routes/about')
 
 // Init express
 var app = express();
@@ -31,6 +35,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter)
+app.use('/rooms', roomsRouter)
+app.use('/news', newsRouter)
+app.use('/meals', mealsRouter)
+app.use('/about', aboutRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
