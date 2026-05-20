fc79f05cdfb5b0f925b17e4900bbad58d9ea213e
Jeff Levine <jlevine84@yahoo.com>
2026-05-20 11:20:46 -0400

    Module 3 completed baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,10 @@
+// Var init
+var fs = require('fs')
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'))
+
 /* Get travel view */
 const travel = (req, res) => {
-    res.render('travel', {title: 'Travlr Getaways'})
+    res.render('travel', {title: 'Travlr Getaways', trips})
 }
 
 module.exports = { travel }
