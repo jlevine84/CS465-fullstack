9edf7e44345e1c1ff1e6ae7c1e5da83e49c70345
Jeff Levine <jlevine84@yahoo.com>
2026-06-14 16:53:19 -0400

    New auth service, user and auth response models, and updated trip data server

---
+++ app_admin/src/app/services/authentication.ts
@@ -0,0 +1,99 @@
+import { Service, Injectable, Inject } from '@angular/core';
+import { BROWSER_STORAGE } from '../storage';
+import { User } from '../models/user';
+import { AuthResponse } from '../models/auth-response';
+import { TripData } from './trip-data';
+
+// Define Auth service
+export class Authentication {
+    // Storage and service access setup
+    constructor(
+        @Inject(BROWSER_STORAGE) private storage: Storage,
+        private tripData: TripData
+    ) {}
+
+    // Variable to handle Auth responses
+    authRes: AuthResponse = new AuthResponse()
+
+    // Method for retrieval of token from Storage
+    public getToken(): string {
+        let out: any
+        out = this.storage.getItem("travlr-token")
+
+        // If no token, return an empty string
+        if (!out) { return "" }
+
+        // If token, return it
+        return out
+    }
+
+    // Method to save token from Storage 
+    public saveToken(token: string): void {
+        this.storage.setItem("travlr-token", token)
+    }
+
+    // Method to logout of application and remove token from storage
+    public logout(): void {
+        this.storage.removeItem("travlr-token")
+    }
+
+    /* Accessors and Mutators */
+    // Method for verifying if a user is logged in and token is still valid
+    public isLoggedIn(): boolean {
+        const currToken: string = this.getToken()
+        
+        // Check if there is a token 
+        if (currToken) {
+            // Has current token, check if it is expired
+            const payload = JSON.parse(atob(currToken.split(".")[1]))
+            return payload.exp > (Date.now() / 1000)
+        } else {
+            // No token
+            return false
+        }
+    }
+
+    // Retrieve current user
+    public getCurrentUser(): User {
+        const token: string = this.getToken()
+        const { email, name } = JSON.parse(atob(token.split(".")[1]))
+
+        return { email, name } as User
+    }
+
+    // Login method 
+    public login(user: User, password: string): void {
+        this.tripData.login(user, password).subscribe({
+            next: (value: any)=> {
+                // If the user was logged in, set vals
+                if (value) {
+                    console.log(value)
+                    this.authRes = value
+                    this.saveToken(this.authRes.token)
+                }
+            }, 
+            error: (error: any)=> {
+                // Catch error if one occurs
+                console.log("Error: " + error)
+            }
+        })
+    }
+
+    // Register method
+    public register(user: User, password: string): void {
+        this.tripData.register(user, password).subscribe({
+            next: (value: any)=> {
+                // If the user was logged in, set vals
+                if (value) {
+                    console.log(value)
+                    this.authRes = value
+                    this.saveToken(this.authRes.token)
+                }
+            }, 
+            error: (error: any)=> {
+                // Catch error if one occurs
+                console.log("Error: " + error)
+            }
+        })
+    }
+}
--- app_admin/src/app/services/trip-data.ts
+++ app_admin/src/app/services/trip-data.ts
@@ -2,7 +2,10 @@ import { Service } from '@angular/core';
 import { Trip } from "../models/trip"
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
+import { User } from '../models/user';
+import { AuthResponse } from '../models/auth-response';
+import { BROWSER_STORAGE } from '../storage';
 
 @Injectable({
     providedIn: "root"
@@ -10,22 +13,52 @@ import { Injectable } from '@angular/core';
 
 export class TripData {
 
-    constructor(private http: HttpClient) {}
-    url = "http://localhost:3000/api/trips"
+    constructor(
+        private http: HttpClient,
+        @Inject(BROWSER_STORAGE) private storage: Storage
+    ) {}
 
+    baseUrl = "http://localhost:3000/api"
+
+    /* Trip endpoints */
+    // All trips
     getTrips() : Observable<Trip[]> {
-        return this.http.get<Trip[]> (this.url)
+        return this.http.get<Trip[]> (this.baseUrl + "/trips")
     }
 
+    // Add a new trip
     addTrip(formData: Trip): Observable<Trip> {
-        return this.http.post<Trip> (this.url, formData)
+        return this.http.post<Trip> (this.baseUrl + "/trips", formData)
     }
 
+    // Get a single trip
     getTrip(tripCode: string): Observable<Trip[]> {
-        return this.http.get<Trip[]> (this.url + "/" + tripCode)
+        return this.http.get<Trip[]> (this.baseUrl + "/trips/" + tripCode)
     }
 
+    // Update a trip
     updateTrip(formData: Trip): Observable<Trip> {
-        return this.http.put<Trip> (this.url + "/" + formData.code, formData)
+        return this.http.put<Trip> (this.baseUrl + "/trips/" + formData.code, formData)
+    }
+
+    // Helper method to process the login and register methods
+    handleAuthAPICall(endpoint: string, user: User, password: string): Observable<AuthResponse> {
+        let formData = {
+            name: user.name,
+            email: user.email,
+            password: password
+        }
+
+        return this.http.post<AuthResponse>(this.baseUrl + "/" + endpoint, formData)
+    }
+
+    // Call to login endpoint: Get JWT- 
+    login(user: User, password: string): Observable<AuthResponse> {
+        return this.handleAuthAPICall("login", user, password)
+    }
+
+    // Call to register endpoint: Create a user and get JWT
+    register(user: User, password: string): Observable<AuthResponse> {
+        return this.handleAuthAPICall("register", user, password)
     }
 }
