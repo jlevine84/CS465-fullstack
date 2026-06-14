0166ee4adc18c5d33012b47abdd0f52ff86887db
Jeff Levine <jlevine84@yahoo.com>
2026-06-13 21:05:00 -0400

    working jwt authentication for put/post methods

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,9 +1,47 @@
 // Reqs and var init
 const express = require("express")
 const router = express.Router()
+const jwt = require("jsonwebtoken")
 
 const tripsController = require("../controllers/trips")
 const authController = require("../controllers/authentication")
+
+// Method for autheticating JWT
+function authenticateJWT(req, res, next) {
+    // console.log('In Middleware')
+    const authHeader = req.headers['authorization']
+    
+    // console.log('Auth Header: ' + authHeader)
+    if(authHeader == null) {
+        console.log('Auth Header Required but NOT PRESENT!')
+        return res.sendStatus(401)
+    }
+
+    let headers = authHeader.split(' ');
+    if (headers.length < 1) {
+        console.log('Not enough tokens in Auth Header: ' +
+        headers.length)
+        return res.sendStatus(501)
+    }
+    
+    const token = authHeader.split(' ')[1];
+    // console.log('Token: ' + token)
+
+    if (token == null) {
+        console.log('Null Bearer Token')
+        return res.sendStatus(401)
+    }
+
+    // console.log(process.env.JWT_SECRET);
+
+    // console.log(jwt.decode(token));
+    const verified = jwt.verify(token, process.env.JWT_SECRET, (err, verified) => {
+        if (err) { return res.sendStatus(401).json('Token Validation Error!') }
+        req.auth = verified; // Set the auth param to the decoded object
+    })
+
+    next(); // We need to continue or this will hang forever
+}
 
 // User registration 
 router.route("/register")
@@ -13,15 +51,14 @@ router.route("/register")
 router.route("/login")
     .post(authController.login) // POST: Login a user
 
-
 // Trip Routes
 router.route("/trips")
     .get(tripsController.tripsList) // GET: Get all trips
-    .post(tripsController.tripsAddTrip) // POST: Add a new trip
+    .post(authenticateJWT, tripsController.tripsAddTrip) // POST: Add a new trip
 
 // Trip Routes by trip code 
 router.route("/trips/:tripCode")
     .get(tripsController.tripsFindByCode) // GET: Single trip by code
-    .put(tripsController.tripsUpdateTrip) // PUT: Update single trip
+    .put(authenticateJWT, tripsController.tripsUpdateTrip) // PUT: Update single trip
 
 module.exports = router
